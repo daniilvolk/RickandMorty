350993ec80b7b5d506292b6f7044fc03528205f3
Dani <inc77740@gmail.com>
2023-06-13 14:55:08 +0300

    Added docstrings and API related files

---
+++ RickAndMorty/ApiClient/RMRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Даниил Волк on 13/06/2023.
+//
+
+import Foundation
+
+
+/// Object that represent a single API call
+final class RMRequest{
+    
+}
